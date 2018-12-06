510a65ddf3cd3e389a9661053421acf0c46a187b
gr8fuldwg <gr8fuldwg@gmail.com>
2018-12-05 20:18:52 -0500

    updated with uuid in mongo note model

--- server/models/note.js
+++ server/models/note.js
@@ -1,6 +1,11 @@
 
 const mongoose = require('mongoose');
+const uuid = require('uuid');
 const noteSchema = new mongoose.Schema({
+  id: {
+    type: String,
+    default: uuid.v4
+  },
   text: {
     type: String,
     required: true
